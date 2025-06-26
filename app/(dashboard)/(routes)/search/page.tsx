f1745438e86ad02bf447c5204efe4480d54761fc
Darknight18-Code <ravindrasingh23693@gmail.com>
2025-06-27 00:16:33 +0530

    bugfix6.5

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -7,17 +7,21 @@ import PageContent from "./_components/page-content";
 import AppliedFilters from "./_components/applied-filters";
 
 interface SearchProps{
-    searchParams:{
-        title : string;
-        categoryId : string;
-        createdAtFilter : string;
-        shiftTiming : string;
-        workMode : string;
-        yearsOfExperience : string;
-    }
+    // Change searchParams to be a Promise of the object
+    searchParams: Promise<{
+        title?: string;
+        categoryId?: string;
+        createdAtFilter?: string;
+        shiftTiming?: string;
+        workMode?: string;
+        yearsOfExperience?: string;
+    }>
 }
 
 const SearchPage = async ({searchParams} : SearchProps) => {
+
+    // Await searchParams here
+    const awaitedSearchParams = await searchParams;
 
     const categories = await db.category.findMany({
         orderBy : {
@@ -26,28 +30,28 @@ const SearchPage = async ({searchParams} : SearchProps) => {
     })
 
     const {userId} = await auth();
-    const jobs  = await getJobs({...searchParams})
+    // Use the awaitedSearchParams
+    const jobs  = await getJobs({...awaitedSearchParams})
 
-    
 
-  return (
-    <>
-        <div className="px-6 pt-6 block md:hidden max-lg:mb-0 ">
-            <SearchContainer/>
-        </div>
+    return (
+        <>
+            <div className="px-6 pt-6 block md:hidden max-lg:mb-0 ">
+                <SearchContainer/>
+            </div>
 
-        <div className="p-6">
-            {/* categories */}
-            <CategoriesList categories={categories}/>
+            <div className="p-6">
+                {/* categories */}
+                <CategoriesList categories={categories}/>
 
-            {/* applied filters */}
-            <AppliedFilters categories={categories}/>
+                {/* applied filters */}
+                <AppliedFilters categories={categories}/>
 
-            {/* page content */}
-            <PageContent jobs={jobs} userId={userId}/>
-        </div>
-    </>
-  )
+                {/* page content */}
+                <PageContent jobs={jobs} userId={userId}/>
+            </div>
+        </>
+    )
 }
 
-export default SearchPage
+export default SearchPage;
